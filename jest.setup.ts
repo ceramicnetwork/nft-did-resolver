de88be94aa1241e509c51995933fb0614503da2d
Sergey Ukustov <sergey@ukstv.me>
2022-04-11 13:57:07 +0300

    chore: Update deps

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,6 @@
-import { GlobalWithFetchMock, default as jestFetchMock } from 'jest-fetch-mock'
+import { jest } from '@jest/globals'
+import { GlobalWithFetchMock } from 'jest-fetch-mock'
+import jestFetchMock from 'jest-fetch-mock'
 
 const customGlobal = global as unknown as GlobalWithFetchMock
 customGlobal.fetch = jestFetchMock
